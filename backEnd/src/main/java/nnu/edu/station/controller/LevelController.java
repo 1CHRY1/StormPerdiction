3b4fdb26c85964bcbeb5a2af9c26350ec6bf6337
chry <2217173586@qq.com>
2024-02-23 10:29:29 +0800

    api

--- backEnd/src/main/java/nnu/edu/station/controller/LevelController.java
+++ backEnd/src/main/java/nnu/edu/station/controller/LevelController.java
@@ -4,61 +4,55 @@ import nnu.edu.station.common.result.JsonResult;
 import nnu.edu.station.common.result.ResultUtils;
 import nnu.edu.station.service.LevelService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.swing.text.html.HTMLDocument;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Level")
+@RequestMapping("api/v1/data/level")
 public class LevelController {
 
     @Autowired
     LevelService levelService;
 
-    @RequestMapping(value = "/getAllInfoByStation/{station}", method = RequestMethod.GET)
-    public JsonResult getAllInfoByStation(@PathVariable String station) {
+    @GetMapping(value = "/station")
+    public JsonResult getAllInfoByStation(@RequestParam String station) {
         /* 根据名称获取站点所有潮位预报信息 */
         return ResultUtils.success(levelService.getAllInfoByStation(station));
     }
 
-    @RequestMapping(value = "/getNoTyph72ByStation/{station}", method = RequestMethod.GET)
-    public JsonResult getNoTyph72ByStation(@PathVariable String station) {
-        /* 根据名称获取站点无台风情况下72小时手动计算潮位预报信息 */
+    @GetMapping(value = "/station/notyph/72")
+    public JsonResult getNoTyph72ByStation(@RequestParam String station) {
+        /* 根据名称获取站点无台风情况下72小时潮位预报信息 */
         return ResultUtils.success(levelService.getNoTyph72ByStation(station));
     }
 
-    @RequestMapping(value = "/getTyph72ByStation/{station}", method = RequestMethod.GET)
+    @GetMapping(value = "/station/typh/72")
     public JsonResult getTyph72ByStation(@PathVariable String station) {
         /* 根据名称获取站点遭遇台风情况下72小时潮位预报信息 */
         return ResultUtils.success(levelService.getTyph72ByStation(station));
     }
 
-    @RequestMapping(value = "/getNoTyphAllByStation/{station}", method = RequestMethod.GET)
+    @GetMapping(value = "/station/notyph/all")
     public JsonResult getNoTyphAllByStation(@PathVariable String station){
         /* 根据名称获取站点无台风情况下所有潮位预报信息 */
         return ResultUtils.success(levelService.getNoTyphAllByStation(station));
     }
 
-    @RequestMapping(value =
-
-
-            "/getNoTyph72ManualByStation/{station}", method = RequestMethod.GET)
+    @GetMapping(value ="/station/notyph/72/manual")
     public JsonResult getNoTyph72ManualByStation(@PathVariable String station) {
         /* 根据名称获取站点无台风情况下72小时手动计算潮位预报信息 */
         return ResultUtils.success(levelService.getNoTyph72ManualByStation(station));
     }
 
-    @RequestMapping(value = "/getTyph72ManualByStation/{station}", method = RequestMethod.GET)
+    @GetMapping(value = "/station/typh/72/manual")
     public JsonResult getTyph72ManualByStation(@PathVariable String station) {
         /* 根据名称获取站点无台风情况下72小时手动计算潮位预报信息 */
         return ResultUtils.success(levelService.getTyph72ManualByStation(station));
     }
 
-    @RequestMapping(value = "/getNoTyphAllManualByStation/{station}", method = RequestMethod.GET)
+    @GetMapping(value = "/station/notyph/all/manual")
     public JsonResult getNoTyphAllManualByStation(@PathVariable String station){
         /* 根据名称获取站点无台风情况下所有手动计算潮位预报信息 */
         return ResultUtils.success(levelService.getNoTyphAllManualByStation(station));
--- backEnd/src/main/java/nnu/edu/station/service/impl/NCServiceImpl.java
+++ backEnd/src/main/java/nnu/edu/station/service/impl/NCServiceImpl.java
@@ -5,6 +5,7 @@ import nnu.edu.station.service.NCService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -14,7 +15,12 @@ public class NCServiceImpl implements NCService {
     NCMapper ncMapper;
 
     @Override
-    public Map<String, String> getInfoByTimeAndType(String time, Integer type) {
+    public List<String> getAll() {
+        return ncMapper.getAll();
+    }
+
+    @Override
+    public Map<String, String> getInfoByTimeAndType(String time, String type) {
         return ncMapper.getInfoByTimeAndType(time, type);
     }
 }
