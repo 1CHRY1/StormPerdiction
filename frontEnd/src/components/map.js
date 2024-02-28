0cca6f1ba03049c70b38a1286a56e3c17aa861fa
kxh4892636 <kxh4892636@gmail.com>
2024-02-28 16:43:08 +0800

    feat: import flowfiled

--- frontEnd/src/components/map.js
+++ frontEnd/src/components/map.js
@@ -1,43 +1,42 @@
-import mapboxgl, { Map } from "mapbox-gl"
+import mapboxgl, { Map } from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
-import FlowLayer from "./flowlayer-mapbox.js"
-import WindLayer from "./windlayer-mapbox.js"
-import WaterDifLayer from "./waterDifLayer-mapbox.js"
-
+import FlowLayer from './flowlayer-mapbox'
+import WaterDifLayer from './waterDifLayer-mapbox'
+import WindLayer from './windlayer-mapbox'
 
 // import Stats from 'https://threejs.org/examples/jsm/libs/stats.module.js'
 
 export function viewing() {
+  // let stats
 
-    // let stats
+  mapboxgl.accessToken =
+    'pk.eyJ1IjoieWNzb2t1IiwiYSI6ImNrenozdWdodDAza3EzY3BtdHh4cm5pangifQ.ZigfygDi2bK4HXY1pWh-wg'
 
-    mapboxgl.accessToken = 'pk.eyJ1IjoieWNzb2t1IiwiYSI6ImNrenozdWdodDAza3EzY3BtdHh4cm5pangifQ.ZigfygDi2bK4HXY1pWh-wg';
+  let ORIGIN = [121.49614, 25.056555, -20]
 
-    let ORIGIN = [121.496140, 25.056555, -20];
+  var map = new Map({
+    container: 'map',
+    // style: 'mapbox://styles/mapbox/light-v9', // style URL
+    style: 'mapbox://styles/ycsoku/cldjl0d2m000501qlpmmex490', // style URL
+    // style: 'mapbox://styles/ycsoku/clrjfv4jz00pe01pdfxgshp6z',
+    center: [120.980697, 32.684162], // starting position [lng, lat]
+    zoom: 5,
+    maxZoom: 20,
+    antialias: true,
+    projection: 'mercator',
+  })
 
-    var map = new Map({
-        container: 'map',
-        // style: 'mapbox://styles/mapbox/light-v9', // style URL
-        style: "mapbox://styles/ycsoku/cldjl0d2m000501qlpmmex490", // style URL
-        // style: 'mapbox://styles/ycsoku/clrjfv4jz00pe01pdfxgshp6z',
-        center: [120.980697, 32.684162], // starting position [lng, lat]
-        zoom: 5,
-        maxZoom: 20,
-        antialias: true,
-        projection: 'mercator'
-    })
+  map.on('load', () => {
+    console.log(WaterDifLayer)
+    console.log(WindLayer)
+    console.log(FlowLayer)
+    ////// main //////////
+    map.addLayer(new WaterDifLayer())
+    // map.addLayer(new FlowLayer())
+    // map.addLayer(new WindLayer())
+  })
 
-    map.on('load', () => {
-
-        ////// main //////////
-        // map.addLayer(new WaterDifLayer())   
-        // map.addLayer(new FlowLayer())
-        map.addLayer(new WindLayer())
-
-    })
-
-    map.on('render', () => {
-
-        // stats?.update()
-    })
+  map.on('render', () => {
+    // stats?.update()
+  })
 }
--- frontEnd/test.ts
+++
@@ -1,8 +0,0 @@
-fetch('http://www.nmc.cn/publish/satellite/FY4A-true-color.htm')
-  .then((res) => {
-    console.log(res.headers)
-    return res.text()
-  })
-  .then((html) => {
-    const result = []
-  })
