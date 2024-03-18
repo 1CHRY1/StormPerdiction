af1ae53380ffb53c348cc6bea71801ad7a8e8086
1CHRY1 <119379635+1CHRY1@users.noreply.github.com>
2024-03-18 15:32:04 +0800

    UpdateData time

--- data/DataProcess/Clawing/DeleteData.py
+++ data/DataProcess/Clawing/DeleteData.py
@@ -47,8 +47,8 @@ def remove_dbcontent(db_path, name):
     conn.close()
 
 # 调用函数并传入文件夹路径
-folder_path = "D:/1study/Work/2023_12_22_Storm/stormPrediction/data/气象产品"
-db_path = "D:/1study/Work/2023_12_22_Storm/stormPrediction/data/DataProcess/Clawing/Meteorology.db"
+folder_path = "D:/1study/Work/2023_12_22_Storm/StormPerdiction/data/气象产品"
+db_path = "D:/1study/Work/2023_12_22_Storm/StormPerdiction/data/DataProcess/Clawing/Meteorology.db"
 # 删除当前日期前天的数据
 delete_jpg_files(folder_path)
 remove_dbcontent(db_path, "Meteorology")
