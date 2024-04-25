bf041efebfd602e216eca9f5da3223c1e7e0c758
chry <2217173586@qq.com>
2024-04-25 19:23:23 +0800

    delete

--- frontEnd/src/store/mapStore.ts
+++
@@ -1,9 +0,0 @@
-// stores/counter.js
-import mapboxgl from 'mapbox-gl'
-import { defineStore } from 'pinia'
-import { Ref, ref } from 'vue'
-
-export const useMapStore = defineStore('map', () => {
-  const map: Ref<null | mapboxgl.Map> = ref(null)
-  return { map }
-})
